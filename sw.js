ac850586638c83b92ca14362980927a0810b1505
DONGHUN <hdhqooo@gmail.com>
2026-05-26 09:08:09 +0900

    Initial commit: HSK 학습 앱 복제본

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'hsk4-v13';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/data/testSets.json',
+  '/data/secretNotes.json',
+  '/data/hskWords.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network first, fallback to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
